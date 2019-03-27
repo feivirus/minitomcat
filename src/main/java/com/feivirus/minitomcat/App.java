d1f312d2d73d3504ec52f5098668a5cd7fc11f5e
jingtian <jifei.zheng@ideacome.com>
2019-03-27 16:15:34 +0800

    初始化tomcat工程

---
+++ src/main/java/com/feivirus/minitomcat/App.java
@@ -0,0 +1,13 @@
+package com.feivirus.minitomcat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
